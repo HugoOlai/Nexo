5bfef5a2b7eb32d6c2e665e675c814176834b793
Hugo Olaitan <34793283+HugoOlai@users.noreply.github.com>
2022-06-13 07:47:21 -0300

    Aplicativo

---
+++ src/page/App.tsx
@@ -0,0 +1,128 @@
+import Home from './Home';
+import Bar from '../component/bar'
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import style from './style.module.scss';
+import Paper from '@mui/material/Paper';
+import Menu from '../component/IconMenu';
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+import Container from '@mui/material/Container';
+import React, { useState, useEffect} from 'react';
+import Typography from '@mui/material/Typography';
+import CssBaseline from '@mui/material/CssBaseline';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import atualizaListaPerguntas from './atualizaListaPerguntas.js'; 
+
+function App() {
+  const [item, setItem] = useState([{
+    Nome: 'Por qual dessas situações você passou?',
+  },
+  {
+    Nome: 'Problema com voo',
+  }, 
+  {
+    Nome: 'Problema com bagagem',
+  }, 
+  {
+    Nome: 'Problema com reembolso de passagem',
+  }])
+  const [definetamanho, setDefinetamanho] = useState<boolean>(false)
+
+  function Redireciona(Nome: string){
+    console.log(Nome)
+    let listaEncontrada = atualizaListaPerguntas(Nome);
+    setItem(listaEncontrada);
+  }
+
+  function voltar(){
+    setItem([{
+      Nome: 'Por qual dessas situações você passou?',
+    },
+    {
+      Nome: 'Problema com voo',
+    }, 
+    {
+      Nome: 'Problema com bagagem',
+    }, 
+    {
+      Nome: 'Problema com reembolso de passagem',
+    }])
+  }
+
+useEffect(() => {
+  var largura = window.innerWidth;
+
+  if(largura <= 750){
+    var largura = window.innerWidth;
+      setDefinetamanho(true)
+    }else{
+      setDefinetamanho(false)
+    }
+  
+  }, [])
+
+return (
+    <React.Fragment>
+    <CssBaseline />
+      <Bar />
+      <Container maxWidth="xl">
+      <Grid container spacing={2}>
+        <Grid item xs="auto">
+          <div className={style.Menu}>
+            <Menu />
+          </div>
+        </Grid>
+        {definetamanho === true? 
+        <Grid item xs={12}>
+        <Card className={style.Perguntas}>
+            <CardContent className={style.Perguntas}>
+              {item.map((item, index) =>(
+                index !== 0?
+                <Typography variant="body2">
+                  <h3 onClick={()=>Redireciona(item.Nome)}> <Home key={index} Nome ={item.Nome} /></h3>
+                </Typography>:
+                <Typography variant="h4" component="div">
+                  <Home key={index} Nome ={item.Nome} /> 
+                  <br />
+                </Typography>
+              ))}
+              <CardActions>
+                <Button startIcon={<ArrowBackIcon />} color="inherit" onClick={()=>voltar()}>Voltar</Button>
+              </CardActions>
+            </CardContent>
+            
+        </Card>
+        </Grid>
+        :
+        <Grid item xs={6}>
+        <Card className={style.Perguntas}>
+            <CardContent className={style.Perguntas}>
+              {item.map((item, index) =>(
+                index !== 0?
+                <Typography variant="body2">
+                  <h3 onClick={()=>Redireciona(item.Nome)}> <Home key={index} Nome ={item.Nome} /></h3>
+                </Typography>:
+                <Typography variant="h4" component="div">
+                  <Home key={index} Nome ={item.Nome} /> 
+                  <br />
+                </Typography>
+              ))}
+              <CardActions>
+                <Button startIcon={<ArrowBackIcon />} color="inherit" onClick={()=>voltar()}>Voltar</Button>
+              </CardActions>
+            </CardContent>
+            
+        </Card>
+        </Grid>
+        }
+      </Grid>
+      </Container>
+  </React.Fragment>
+
+  );
+}
+
+export default App;
